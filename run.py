503589732ea32f9715be5de3a6df4aa09ce9e8ce
ROZHAK <76166352+R0ZHAK@users.noreply.github.com>
2021-07-02 12:35:04 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os,sys
+
+### Salah Decompile Bang :v
+
+#####################################
+os.system('cd anjing')
+os.system('python2 g_o_b_l_o_k.pyc')
+#####################################
