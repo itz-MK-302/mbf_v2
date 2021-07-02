1c3989d8ac3ecf2f5f51db1903b471b119b41f51
ROZHAK <76166352+R0ZHAK@users.noreply.github.com>
2021-07-02 06:03:45 +0000

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-
-### Salah Decompile Bang :v
-
-#####################################
-os.system('cd anjing')
-os.system('python2 g_o_b_l_o_k.pyc')
-#####################################
